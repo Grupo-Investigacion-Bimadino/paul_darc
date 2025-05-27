62a3f2f7b9c79a6e521aeb4505f264ab625f3dfe
marloncobos001 <marloncobos001@gmail.com>
2025-05-27 15:32:11 +0000

    Actualizacion de Schemas

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema, User } from './schemas/users.schema';
+
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
