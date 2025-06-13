77157a554968697c17c1f9a0e615979e3920ee62
marloncobos001 <marloncobos001@gmail.com>
2025-06-13 20:38:15 +0000

    cambios

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // 1. Importar MongooseModule
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UsersSchema, Users } from './schemas/users.schema';
-
+import { Users, UsersSchema } from './schemas/users.schema'; // 2. Importar el Modelo y el Schema
 
 @Module({
-  imports:[
+  imports: [
+    // 3. Registrar el modelo en el módulo
     MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]),
   ],
   controllers: [UsersController],
   providers: [UsersService],
+  // 4. Exportar el servicio si otros módulos lo necesitan (muy común para la autenticación)
+  exports: [UsersService],
 })
 export class UsersModule {}
