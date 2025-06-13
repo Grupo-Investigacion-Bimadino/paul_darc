989a06f134c9897eda0181c8fb49cbfe3af32896
marloncobos001 <marloncobos001@gmail.com>
2025-06-13 19:45:25 +0000

    cambios

--- src/regions/regions.module.ts
+++ src/regions/regions.module.ts
@@ -1,8 +1,15 @@
+// Contenido CORRECTO para src/regions/regions.module.ts
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // <-- 1. Importa MongooseModule
 import { RegionsService } from './regions.service';
 import { RegionsController } from './regions.controller';
+import { Region, RegionSchema } from './schemas/region.schema'; // <-- 2. Importa el modelo y el schema
 
 @Module({
+  imports: [
+    // 3. Registra el modelo aquí
+    MongooseModule.forFeature([{ name: Region.name, schema: RegionSchema }]),
+  ],
   controllers: [RegionsController],
   providers: [RegionsService],
 })
